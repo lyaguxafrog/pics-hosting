442429d90e4d790645721e50a0cee4caa3c313fa
adrian <maccree.a@yandex.ru>
2023-10-18 00:06:34 +0300

    db connect

--- bot/bot/bot_app.py
+++ bot/bot/bot_app.py
@@ -1,51 +1,41 @@
-# -*- coding: utf-8 -*-
-
-
 import telebot
-
-
 import psycopg2
-from psycopg2 import sql
-import select
 
 import os
 from dotenv import load_dotenv, find_dotenv
 
-from services.get_tokens import tokens
-
 load_dotenv(find_dotenv())
 
-dbname = os.getenv("POSTGRES_DB")
-password = os.getenv("POSTGRES_PASSWORD")
-user = os.getenv("POSTGRES_USER")
+# Функция для установления соединения с базой данных и получения активных токенов
+def get_active_bot_tokens():
+    conn = psycopg2.connect(
+        dbname=os.getenv("POSTGRES_DB"),
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD"),
+        host="db",
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT bot_token FROM bot WHERE is_active = TRUE")
+    active_tokens = [row[0] for row in cursor.fetchall()]
+    conn.close()
+    return active_tokens
 
-conn = psycopg2.connect(f"dbname={dbname} user={user} password={password} host=db port=5432")
-conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+# Создаем ботов на основе активных токенов
+active_tokens = get_active_bot_tokens()
+bots = [telebot.TeleBot(token) for token in active_tokens]
 
-cur = conn.cursor()
+# Функция для обработки команды /start
+def handle_start(message):
+    for bot in bots:
+        bot.send_message(message.chat.id, "Привет, я активен!")
 
-cur.execute("LISTEN new_token;")
+# Создаем хендлер для команды /start для каждого бота
+for bot in bots:
+    @bot.message_handler(commands=['start'])
+    def start(message):
+        handle_start(message)
 
-while True:
-    if select.select([conn], [], [], 5) == ([], [], []):
-        pass
-    else:
-        conn.poll()
-        while conn.notifies:
-            notify = conn.notifies.pop(0)
-            print("Received notification:", notify.payload)
-
-
-            token_list = tokens()
-
-            bots = []
-            for token in token_list:
-                bot = telebot.TeleBot(token)
-                bots.append(bot)
-
-                for bot in bots:
-                    @bot.message_handler(func=lambda message: True)
-                    def echo_all(message):
-                        bot.reply_to(message, message.text)
-
-            bot.polling()
+if __name__ == "__main__":
+    for bot in bots:
+        bot.remove_webhook()
+        bot.polling(none_stop=True)
