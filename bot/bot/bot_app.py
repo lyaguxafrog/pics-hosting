076b033dd19bd71c9478cc13bb7494f306ddee75
adrian <maccree.a@yandex.ru>
2023-10-18 23:21:54 +0300

    image process

--- bot/bot/bot_app.py
+++ bot/bot/bot_app.py
@@ -1,63 +1,21 @@
+
 import telebot
 import psycopg2
-from multiprocessing import Process
-from datetime import datetime
 from PIL import Image
 import io
-import os
+from multiprocessing import Process
+from datetime import datetime
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from bot.services.get_tokens import get_active_bot_tokens
 from bot.services.dbconnect import db
 from bot.services.last_login import update_last_login
 from bot.services.saveimage import save_image
-import psycopg2
-from psycopg2 import sql
-from io import BytesIO
-
 
 # Функция для получения списка активных токенов из базы данных
 tokens = get_active_bot_tokens(db_connection=db())
 
 datebase = db()
 db_cursor = datebase.cursor()
-
-
-# Функция для сохранения изображения в базе данных с использованием Large Objects
-def save_image(user_id, name, password, image_data):
-    try:
-        conn = db()
-        cursor = conn.cursor()
-
-        # Создать новый Large Object
-        cursor.execute("SELECT lo_create(0)")
-        image_oid = cursor.fetchone()[0]
-
-        # Откройте Large Object для записи
-        cursor.execute(sql.SQL("SELECT lo_open(%s, %s)"), (image_oid, psycopg2.extensions.LO_WRONLY))
-        image_file = cursor.fetchone()[0]
-
-        # Запишите данные изображения в Large Object
-        image_data_stream = BytesIO(image_data)
-        while True:
-            data = image_data_stream.read(1024)
-            if not data:
-                break
-            cursor.execute(sql.SQL("SELECT lowrite(%s, %s)"), (image_file, psycopg2.Binary(data)))
-
-        # Закройте Large Object
-        cursor.execute(sql.SQL("SELECT lo_close(%s)"), (image_file,))
-
-        # Вставьте запись о Large Object в таблицу
-        cursor.execute("INSERT INTO pictures (name, pic_oid, owner_id, password) VALUES (%s, %s, %s, %s)",
-                       (name, image_oid, str(user_id), password))
-
-        conn.commit()
-    except Exception as e:
-        conn.rollback()
-        print(f"Error: {e}")
-    finally:
-        cursor.close()
-        conn.close()
 
 # Функция, которая будет выполняться для каждого активного бота
 def start_bot(token):
@@ -119,11 +77,52 @@ def start_bot(token):
             file_path = file_info.file_path
             image_data = bot_instance.download_file(file_path)
 
-            # Сохраните изображение в базе данных с использованием Large Objects
-            save_image(user_id, image_name, image_password, image_data)
+            # Сохранить изображение в базе данных
+            image_data = io.BytesIO(image_data)
+            image = Image.open(image_data)
+            image_format = "PNG"  # Используем формат PNG
+            image.save(image_data, format=image_format)
+
+            image_data.seek(0)  # Сбросить указатель потока
+
+            # Сохранить изображение в базе данных
+            save_image(user_id, image_name, image_password, image_data.read())
             bot_instance.send_message(user_id, "Изображение сохранено в базе данных.")
         else:
             bot_instance.send_message(user_id, "Пожалуйста, отправьте изображение.")
+
+
+
+
+    @bot_instance.message_handler(commands=['viewimages'])
+    def handle_viewimages(message):
+        user_id = message.from_user.id
+        db_cursor.execute("SELECT id, name FROM pictures WHERE owner_id = %s", (str(user_id),))
+        saved_images = db_cursor.fetchall()
+
+        if saved_images:
+            markup = InlineKeyboardMarkup(row_width=2)
+
+            for image_id, image_name in saved_images:
+                button = InlineKeyboardButton(image_name, callback_data=f"view_{image_id}")
+                markup.add(button)
+
+            bot_instance.send_message(user_id, "Выберите изображение для просмотра:", reply_markup=markup)
+        else:
+            bot_instance.send_message(user_id, "У вас нет сохраненных изображений.")
+
+    @bot_instance.callback_query_handler(func=lambda call: call.data.startswith("view_"))
+    def callback_view_image(call):
+        user_id = call.from_user.id
+        image_id = call.data.split("_")[1]
+
+        db_cursor.execute("SELECT pic_data FROM pictures WHERE id = %s AND owner_id = %s", (image_id, str(user_id)))
+        image_data = db_cursor.fetchone()
+
+        if image_data:
+            bot_instance.send_photo(user_id, image_data[0])
+        else:
+            bot_instance.send_message(user_id, "Изображение не найдено.")
 
     bot_instance.remove_webhook()
     bot_instance.polling(none_stop=True)
